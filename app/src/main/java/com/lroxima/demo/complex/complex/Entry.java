67af9e691ef5ae6a0f4b8ff3a01cbf95d4ce9cb6
Leon Da Costa <leon@onbrandgroup.co.uk>
2017-09-13 17:18:16 +0100

    Added UI element

--- app/src/main/java/com/lroxima/demo/complex/complex/Entry.java
+++ app/src/main/java/com/lroxima/demo/complex/complex/Entry.java
@@ -1,5 +1,6 @@
 package com.lroxima.demo.complex.complex;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -10,10 +11,13 @@ public class Entry {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Entry() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -23,8 +27,13 @@ public class Entry {
     public String getTitle() {
         return mTitle;
     }
-
     public void setTitle(String mTitle) {
         this.mTitle = mTitle;
     }
+
+    public Date getDate() { return mDate; }
+    public void setDate(Date date) { mDate = date; }
+
+    public boolean isSolved() { return mSolved; }
+    public void setSolved(boolean solved) { mSolved = solved; }
 }
